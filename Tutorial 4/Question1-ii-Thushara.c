aa6e917d1ea0c93fe9b29953a041b0d67eaf6226
Darklaneanjana <darklaneanjana@gmail.com>
2020-11-18 20:05:50 +0530

    Update Question1-ii-Thushara.c

--- Tutorial 4/Question1-ii-Thushara.c
+++ Tutorial 4/Question1-ii-Thushara.c
@@ -1,8 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int main()
-{	
-	printf("Welcome\nto\nSLIIT");
+{
 
-	return 0;
+    //1
+    printf("********\n");
+    for(int i=0;i<4;i++){
+        printf("*      *\n");
+    }printf("********\n");
+
+    //2
+    char a[20] = "*";
+    for(int j=0;j<6;j++){
+        printf("%s\n",a);
+        strcat(a, "*");
+    }
+
+    //3
+    printf("  *\n ***\n*****\n");
+    for(int k=3;k>0;k--){
+        printf("%3s\n","*");
+    }
+
+    return 0;
 }
