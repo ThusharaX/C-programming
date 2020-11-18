cd18560051eae886c64fb72a052c9b0ef0e0cffd
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2020-11-19 00:24:49 +0530

    :bug: Fixed
    Co-Authored-By: Darklaneanjana <23092020+Darklaneanjana@users.noreply.github.com>

--- Tutorial 4/Question2-Anjana.c
+++ Tutorial 4/Question2-Anjana.c
@@ -1,26 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 int main()
 {
-
+	int i;
+	int j;
+	int k;
+	
     //1
     printf("********\n");
-    for(int i=0;i<4;i++){
+    for(i=0;i<4;i++){
         printf("*      *\n");
     }printf("********\n");
 
     //2
     char a[20] = "*";
-    for(int j=0;j<6;j++){
+    for(j=0;j<6;j++){
         printf("%s\n",a);
         strcat(a, "*");
     }
 
     //3
     printf("  *\n ***\n*****\n");
-    for(int k=3;k>0;k--){
+    for(k=3;k>0;k--){
         printf("%3s\n","*");
     }
 
