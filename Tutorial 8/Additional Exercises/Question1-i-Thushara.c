053270540e1b1f0bbafd110e7d06f838ddad4ca1
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2020-11-18 13:05:11 +0530

    :bug: fixed
    Tutorial 8
    :heavy_check_mark: Question1-i
    :heavy_check_mark: Question1-ii

--- Tutorial 8/Additional Exercises/Question1-i-Thushara.c
+++ Tutorial 8/Additional Exercises/Question1-i-Thushara.c
@@ -1,19 +1,26 @@
 #include<stdio.h>
 
+void solidSquare(int);
+
 int main()
 {
-	int side, count1, count2;
+	int side;
 	
 	printf("Side Value : ");
 	scanf("%d", &side);
 	printf("\n");
 	
+	solidSquare(side);
+	
+	return 0;
+}
+
+void solidSquare(int side) {
+	int count1, count2;
 	for (count1 = 1; count1 <= side; count1++){
 		for (count2 = 1; count2 <= side; count2++){
 			printf("*");
 		}
 		printf("\n");
 	}
-	
-	return 0;
 }
