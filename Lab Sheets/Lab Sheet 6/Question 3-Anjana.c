ae033540d17183eddb758b4469fe39cd37417b61
Darklaneanjana <darklaneanjana@gmail.com>
2020-11-27 02:00:30 +0530

    Add files via upload

--- Lab Sheets/Lab Sheet 6/Question 3-Anjana.c
+++ Lab Sheets/Lab Sheet 6/Question 3-Anjana.c
@@ -2,17 +2,15 @@
 
 int main()
 {
-    int a=0,b;
+    int a=0;
     int arr1[3][3];
     int arr2[3][3];
 
     for(int i=0;i<3;i++){//storing to arr1
-        for(int j=0;j<3;j++)
+        for(int j=0;j<3;j++){
             scanf("%d",&arr1[i][j]);
-    }
-    for(int i=0;i<3;i++){//storing transpose of arr1 to arr2
-        for(int j=0;j<3;j++)
-           arr2[j][i]= arr1[i][j];
+            arr2[j][i]= arr1[i][j];//storing the transpose of arr1 to arr2
+        }
     }
 
     return 0;
