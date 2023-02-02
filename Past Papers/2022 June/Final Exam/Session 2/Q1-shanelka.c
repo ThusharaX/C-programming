7eb055afcab7a99895d07d1fbcae3cbcc7ad499b
Shanelka Pramuditha <99pramuditha@gmail.com>
2023-02-02 23:15:00 +0530

    Past Paper Update
    2022/June/S1/Q01

---
+++ Past Papers/2022 June/Final Exam/Session 2/Q1-shanelka.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// start the main
+int main(void) {
+    // local variable declaration
+    int number, digit[4];
+    int i, j; // counter variable
+
+    // get correct number
+    while ((number <= 999) || (number >= 10000)) {
+        printf("Please enter your 4 digit number : ");
+        scanf("%d", &number);
+        if ((number <= 999) || (number >= 10000)) {
+            printf("Invalid Number\n");
+        }
+    }
+
+    // separate to digits
+    digit[0] = number % 10000 / 1000;
+    digit[1] = number % 1000 / 100;
+    digit[2] = number % 100 / 10;
+    digit[3] = number % 10 / 1;
+
+    // check palindrome or not
+    if ((digit[0] == digit[3]) && (digit[1] == digit[2])) {
+        printf("It is a Palindrome");
+    }
+    else {
+        printf("It is not a Palindrome");
+    }
+
+    return 0;
+}
