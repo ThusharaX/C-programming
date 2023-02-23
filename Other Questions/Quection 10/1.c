fc2dc8355be547ac36d080ac88a026eaef67980f
Iresh Eranga <iresheranga2020al@gmail.com>
2023-02-23 19:24:51 +0530

    Quection-10
    1St q

---
+++ Other Questions/Quection 10/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	
+	int i,j,k;
+	
+	for(i=1;i<=2;i++)
+	{
+		for(j=1;j<=2;j++)
+		{
+			for(k=1;k<=2;k++)
+			{
+				printf("[%d %d %d]\n",i,j,k);
+			}
+			
+		}
+	
+	}
+	return 0;
+}
